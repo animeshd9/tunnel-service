39aa9df7703bd921354437848eff51c1a6a13835
animeshd9 <animeshd838@gmail.com>
2024-01-08 02:02:13 +0530

    chore

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const localtunnel = require('../localtunnel');
 
+// Get token from command line arguments
+const args = process.argv.slice(2); // Get command line arguments excluding node executable and script name
+let token = '';
+
+args.forEach(arg => {
+  if (arg.startsWith('--token=')) {
+    token = arg.split('=')[1];
+  }
+});
+if (!token) {
+  console.error('Please provide a valid token using --token={your_token}');
+  process.exit(1);
+}
 
 (async () => {
-    const tunnel = await localtunnel({
-      port: 8080,
-      host: 'https://setside.io',
-      subdomain: 'editor',
-      
-    }).catch(err => {
-      throw err;
-    });
-  
-    tunnel.on('error', err => {
-      throw err;
-    });
-  
-    console.log('your url is: %s', tunnel.url);
-    // console.log(tunnel)
-  
-    /**
-     * `cachedUrl` is set when using a proxy server that support resource caching.
-     * This URL generally remains available after the tunnel itself has closed.
-     * @see https://github.com/localtunnel/localtunnel/pull/319#discussion_r319846289
-     */
-    if (tunnel.cachedUrl) {
-      console.log('your cachedUrl is: %s', tunnel.cachedUrl);
-    }
-  
-      tunnel.on('request', info => {
-        console.log(new Date().toString(), info.method, info.path);
-      });
-  })();
+  const tunnel = await localtunnel({
+    port: 8080,
+    host: 'http://setside.io',
+    subdomain: 'editor',
+    token: token // Set the token obtained from command line argument
+  }).catch(err => {
+    console.log(err);
+    throw err;
+  });
+
+  tunnel.on('error', err => {
+    throw err;
+  });
+
+  console.log('your url is: %s', tunnel.url);
+
+  if (tunnel.cachedUrl) {
+    console.log('your cachedUrl is: %s', tunnel.cachedUrl);
+  }
+
+  tunnel.on('request', info => {
+    console.log(new Date().toString(), info.method, info.path);
+  });
+})();
