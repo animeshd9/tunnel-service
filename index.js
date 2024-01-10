3ed64ab46402cd828769d253d178c5352bc73b8c
animeshd9 <animeshd838@gmail.com>
2024-01-10 14:54:48 +0530

    working fine

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const localtunnel = require('./localtunnel');
 require('dotenv').config()
 // Get token from command line arguments
 const args = process.argv.slice(2); // Get command line arguments excluding node executable and script name
-let token = process.env.TOKEN;
-// let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldBVjRwYy1SQW1pV08yN3JDR1liVE1admRKWSJ9.eyJhdWQiOiIzMGU3NGYzMy1mOWIwLTRlMmMtYjk4YS1jNDg0NWY2NTQzYmUiLCJleHAiOjE3MDQ4MTMwNzQsImlhdCI6MTcwNDc3NzA3NCwiaXNzIjoiYWNtZS5jb20iLCJzdWIiOiJlNDNiNzA4MS1jMjg0LTQ3Y2YtOWVmMy1kYmQ2NWQ5MTI2YTQiLCJqdGkiOiJmYzBkMTUwMy1hY2Y5LTRhYTgtYmFkMi0wODhjN2VmNDFjZTgiLCJhdXRoZW50aWNhdGlvblR5cGUiOiJQQVNTV09SRCIsImVtYWlsIjoiYW5pbWVzaGQ4MzhAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImFwcGxpY2F0aW9uSWQiOiIzMGU3NGYzMy1mOWIwLTRlMmMtYjk4YS1jNDg0NWY2NTQzYmUiLCJyb2xlcyI6W10sImF1dGhfdGltZSI6MTcwNDc3NzA3NCwidGlkIjoiOTZmNjUxZDQtNDdiMS1kYTIyLTgyY2UtYzBhODliNWVlMWEwIiwidXNlcklkIjoiNjU0MzRhZGVkMGYyYjUyMWVhNThkNDBkIn0.BoFeHeRwAxc9h2kunumC0wYI8o2MZziQvmE7kmyW4yx7YtFwwX7FPO9GYdgHeGtyWMpGHMEmxAKFsFjRmx0lAuJHKoejZtHL_ncmewQnYiYeQ2ZOHTTCHFEFYr5uUJy_KlS5IZFDKUpNdeuLaB-ryvOfENwckT26P33XCEsIwOq4B3C-Yg_stwMjAT1J-JHfpeYfI5Abq2z7RBTfqHX1tgzDw6ohBu4y5MZ5Kavjk5f2zsK52GPUoLgStNXhxwnZyIStje9V_NnbgobpTKpjhGC36Ju-RXLqVlh1JbcKAdf2n9PlfEpEGtTsEsU67j4C2nuoX-BBrfYozY_AplgFrg'
+// let token = process.env.TOKEN;
+let token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IldBVjRwYy1SQW1pV08yN3JDR1liVE1admRKWSJ9.eyJhdWQiOiIzMGU3NGYzMy1mOWIwLTRlMmMtYjk4YS1jNDg0NWY2NTQzYmUiLCJleHAiOjE3MDQ4MTMwNzQsImlhdCI6MTcwNDc3NzA3NCwiaXNzIjoiYWNtZS5jb20iLCJzdWIiOiJlNDNiNzA4MS1jMjg0LTQ3Y2YtOWVmMy1kYmQ2NWQ5MTI2YTQiLCJqdGkiOiJmYzBkMTUwMy1hY2Y5LTRhYTgtYmFkMi0wODhjN2VmNDFjZTgiLCJhdXRoZW50aWNhdGlvblR5cGUiOiJQQVNTV09SRCIsImVtYWlsIjoiYW5pbWVzaGQ4MzhAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImFwcGxpY2F0aW9uSWQiOiIzMGU3NGYzMy1mOWIwLTRlMmMtYjk4YS1jNDg0NWY2NTQzYmUiLCJyb2xlcyI6W10sImF1dGhfdGltZSI6MTcwNDc3NzA3NCwidGlkIjoiOTZmNjUxZDQtNDdiMS1kYTIyLTgyY2UtYzBhODliNWVlMWEwIiwidXNlcklkIjoiNjU0MzRhZGVkMGYyYjUyMWVhNThkNDBkIn0.BoFeHeRwAxc9h2kunumC0wYI8o2MZziQvmE7kmyW4yx7YtFwwX7FPO9GYdgHeGtyWMpGHMEmxAKFsFjRmx0lAuJHKoejZtHL_ncmewQnYiYeQ2ZOHTTCHFEFYr5uUJy_KlS5IZFDKUpNdeuLaB-ryvOfENwckT26P33XCEsIwOq4B3C-Yg_stwMjAT1J-JHfpeYfI5Abq2z7RBTfqHX1tgzDw6ohBu4y5MZ5Kavjk5f2zsK52GPUoLgStNXhxwnZyIStje9V_NnbgobpTKpjhGC36Ju-RXLqVlh1JbcKAdf2n9PlfEpEGtTsEsU67j4C2nuoX-BBrfYozY_AplgFrg'
 let port = process.env.PORT;
 let name = process.env.NAME;
-console.log(token)
+console.log(name)
 // console.log('---port:',port,'---')
 
 args.forEach(arg => {
@@ -24,10 +24,10 @@ if (!token) {
 
 (async () => {
   const tunnel = await localtunnel({
-    port: port,
+    port: 3333,
     host: 'http://setside.io',
     token: token ,// Set the token obtained from command line argument
-    name:name
+    name:'code'
   }).catch(err => {
     console.log(err);
     throw err;
